b04373858a6e0b541ab02391d72f241891b5ab17
NightCandle <instkffff@gmail.com>
2020-04-26 23:06:22 +0800

    .

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const {telegraf} = require('telegraf')
+const {Telegraf} = require('telegraf')
 
-const bot = new telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf()
 
 const text2png = require('text2png')
 
 bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
 	if (inlineQuery.query.length === 0){
-		return url = text2png('Triglavian',{textColor: 'white', font: '20px Triglavian', lineSpacing:3, xpadding:3, ypadding:3, bgColor: '#222c36' })
+		return url = text2png('Triglavian',{
+			textColor: 'white',
+			localFontPath:'./Triglavian.otf', 
+			localFontName: 'Triglavian',
+			font: '20px Triglavian', 
+			lineSpacing:3, 
+			xpadding:3, 
+			ypadding:3, 
+			backgroundColor: '#222c36' })
 	} else {
-		return url = text2png(inlineQuery.query,{textColor: 'white', font: '20px Triglavian', lineSpacing:3, xpadding:3, ypadding:3, bgColor: '#222c36' })
+		return url = text2png('Triglavian',{
+			textColor: 'white',
+			localFontPath:'./Triglavian.otf', 
+			localFontName: 'Triglavian',
+			font: '20px Triglavian', 
+			lineSpacing:3, 
+			xpadding:3, 
+			ypadding:3, 
+			backgroundColor: '#222c36' })
 	}
 
 	let results = []
