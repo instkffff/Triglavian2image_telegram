e1c2807adc1a7ecf0dad5837754e408cbbe09339
NightCandle <instkffff@gmail.com>
2020-04-27 00:37:48 +0800

    .

--- index.js
+++ index.js
@@ -18,18 +18,8 @@ bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
 		backgroundColor: '#222c36'
 	}).catch(error){
 		console.log('error')
-		img = text2png( 'Triglavian',{
-		textColor: 'white',
-		localFontPath:'./Triglavian.otf', 
-		localFontName: 'Triglavian',
-		font: '20px Triglavian', 
-		lineSpacing:3, 
-		xpadding:3, 
-		ypadding:3, 
-		backgroundColor: '#222c36'
-	})
 	}
-
+ 	
 	fs.writeFileSync(`./image/${inlineQuery.query}.png` ,img)
 
 	let url = `http://54.169.114.12:3400/${inlineQuery.query}.png`
