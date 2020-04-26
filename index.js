9ad559682f34b7d00072fff0e55de117771b311b
NightCandle <instkffff@gmail.com>
2020-04-26 23:28:01 +0800

    .

--- index.js
+++ index.js
@@ -5,27 +5,16 @@ const bot = new Telegraf()
 const text2png = require('text2png')
 
 bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
-	if (inlineQuery.query.length === 0){
-		return url = text2png('Triglavian',{
-			textColor: 'white',
-			localFontPath:'./Triglavian.otf', 
-			localFontName: 'Triglavian',
-			font: '20px Triglavian', 
-			lineSpacing:3, 
-			xpadding:3, 
-			ypadding:3, 
-			backgroundColor: '#222c36' })
-	} else {
-		return url = text2png('Triglavian',{
-			textColor: 'white',
-			localFontPath:'./Triglavian.otf', 
-			localFontName: 'Triglavian',
-			font: '20px Triglavian', 
-			lineSpacing:3, 
-			xpadding:3, 
-			ypadding:3, 
-			backgroundColor: '#222c36' })
-	}
+	url = text2png('Triglavian',{
+		textColor: 'white',
+		localFontPath:'./Triglavian.otf', 
+		localFontName: 'Triglavian',
+		font: '20px Triglavian', 
+		lineSpacing:3, 
+		xpadding:3, 
+		ypadding:3, 
+		backgroundColor: '#222c36' })
+	
 
 	let results = []
 
