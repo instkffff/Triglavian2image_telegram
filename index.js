64f96366b71885597029055b073bfdd40f9ed6a5
NightCandle <instkffff@gmail.com>
2020-04-27 00:52:22 +0800

    .

--- index.js
+++ index.js
@@ -7,35 +7,38 @@ const fs = require('fs')
 const text2png = require('text2png')
 
 bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
-	let img = text2png( inlineQuery.query,{
-		textColor: 'white',
-		localFontPath:'./Triglavian.otf', 
-		localFontName: 'Triglavian',
-		font: '20px Triglavian', 
-		lineSpacing:3, 
-		xpadding:3, 
-		ypadding:3, 
-		backgroundColor: '#222c36'
-	}).catch(error){
-		console.log('error')
-	}
- 	
-	fs.writeFileSync(`./image/${inlineQuery.query}.png` ,img)
+	
+	try {
+		let img = text2png (inlineQuery.query,{
+			textColor: 'white',
+			localFontPath:'./Triglavian.otf', 
+			localFontName: 'Triglavian',
+			font: '30px Triglavian', 
+			lineSpacing:5, 
+			xpadding:5, 
+			ypadding:5, 
+			backgroundColor: '#222c36'
+		})
+		fs.writeFileSync(`./image/${inlineQuery.query}.png` ,img)
 
-	let url = `http://54.169.114.12:3400/${inlineQuery.query}.png`
+		let url = `http://54.169.114.12:3400/${inlineQuery.query}.png`
+		results = [{
+			type: 'photo',
+			id: 1,
+			thumb_url: encodeURI(url),
+			photo_url: encodeURI(url)
+		}]
+		return answerInlineQuery(results,{ cache_time: 0})
 
-	console.log(url)
-
-	let results = []
-
-	results = [{
-		type: 'photo',
-		id: 1,
-		thumb_url: encodeURI(url),
-		photo_url: encodeURI(url)
-	}]
-
-	return answerInlineQuery(results,{ cache_time: 0 ,is_personal: True})
+	} catch(error) {
+		results = [{
+			type: 'photo',
+			id: 1,
+			thumb_url: 'http://54.169.114.12:3400/Triglavian.png',
+			photo_url: 'http://54.169.114.12:3400/Triglavian.png'
+		}]
+		return answerInlineQuery(results,{ cache_time: 0})
+	}	
 })
 
 bot.launch()
