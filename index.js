601befe2bf375914b4f8667f4ba2e2c810f70cf7
NightCandle <instkffff@gmail.com>
2020-04-26 23:55:37 +0800

    .

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const {Telegraf} = require('telegraf')
 
 const bot = new Telegraf()
 
+const fs = require('fs')
+
 const text2png = require('text2png')
 
 bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
-	url = text2png('Triglavian',{
+	let img = text2png('inlineQuery.query',{
 		textColor: 'white',
 		localFontPath:'./Triglavian.otf', 
 		localFontName: 'Triglavian',
@@ -13,9 +15,12 @@ bot.on('inline_query', async({ inlineQuery, answerInlineQuery }) => {
 		lineSpacing:3, 
 		xpadding:3, 
 		ypadding:3, 
-		backgroundColor: '#222c36',
-		output: 'dataURL' })
-	
+		backgroundColor: '#222c36'})
+
+	fs.writeFileSync(`./image/${inlineQuery.query}.png` ,img)
+
+	let url = `http://54.169.114.12:3400/image/${inlineQuery.query}.png`
+
 	console.log(url)
 
 	let results = []
